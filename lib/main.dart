9c68103bbc87ec387d57ce3a708c710ce6b76754
Shirshajit Sen Gupta <shirshajit@gmail.com>
2022-11-13 15:01:20 +0800

    home screen

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.lancelotTextTheme(),
-        primarySwatch: Colors.blue,
+        primarySwatch:
+            // ignore: use_full_hex_values_for_flutter_colors
+            MaterialColor(0x09C2AE, getSwatch(const Color(0x09C2AE))),
         appBarTheme: const AppBarTheme(
           backgroundColor: Color.fromRGBO(135, 166, 130, 54),
           elevation: 0,
@@ -43,7 +45,7 @@ class BlocNav extends StatelessWidget {
   const BlocNav({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    context.read<LoginBloc>().add(const LoginNavEvent());
+    context.read<LoginBloc>().add(const InitializeEvent());
     return BlocBuilder<LoginBloc, LoginState>(builder: ((context, state) {
       if (state is LoggedIn) {
         return const ProductsView();
@@ -61,4 +63,37 @@ class BlocNav extends StatelessWidget {
       }
     }));
   }
+}
+
+Map<int, Color> getSwatch(Color color) {
+  final hslColor = HSLColor.fromColor(color);
+  final lightness = hslColor.lightness;
+
+  /// if [500] is the default color, there are at LEAST five
+  /// steps below [500]. (i.e. 400, 300, 200, 100, 50.) A
+  /// divisor of 5 would mean [50] is a lightness of 1.0 or
+  /// a color of #ffffff. A value of six would be near white
+  /// but not quite.
+  const lowDivisor = 6;
+
+  /// if [500] is the default color, there are at LEAST four
+  /// steps above [500]. A divisor of 4 would mean [900] is
+  /// a lightness of 0.0 or color of #000000
+  const highDivisor = 5;
+
+  final lowStep = (1.0 - lightness) / lowDivisor;
+  final highStep = lightness / highDivisor;
+
+  return {
+    50: (hslColor.withLightness(lightness + (lowStep * 5))).toColor(),
+    100: (hslColor.withLightness(lightness + (lowStep * 4))).toColor(),
+    200: (hslColor.withLightness(lightness + (lowStep * 3))).toColor(),
+    300: (hslColor.withLightness(lightness + (lowStep * 2))).toColor(),
+    400: (hslColor.withLightness(lightness + lowStep)).toColor(),
+    500: (hslColor.withLightness(lightness)).toColor(),
+    600: (hslColor.withLightness(lightness - highStep)).toColor(),
+    700: (hslColor.withLightness(lightness - (highStep * 2))).toColor(),
+    800: (hslColor.withLightness(lightness - (highStep * 3))).toColor(),
+    900: (hslColor.withLightness(lightness - (highStep * 4))).toColor(),
+  };
 }
